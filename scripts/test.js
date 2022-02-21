c28747941935ffb9940b06a4d12db396ba4b07e7
PGreaneyLYIT <91680965+PGreaneyLYIT@users.noreply.github.com>
2022-02-21 20:14:31 +0000

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,41 @@
-// ./test.js
-import http from 'k6/http';
-import { sleep } from 'k6';
+// Scenario: Scenario_1 (executor: ramping-vus)
+
+import { sleep } from 'k6'
+import http from 'k6/http'
 
 export const options = {
-  duration: '1m',
-  vus: 50,
-};
+  ext: {
+    loadimpact: {
+      distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+      apm: [],
+    },
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
 
-export default function () {
-  const res = http.get('https://test.k6.io');
-  sleep(1);
+export function scenario_1() {
+  let response
+
+  // get-items test
+  response = http.get('http://ecsalb-309629614.eu-west-1.elb.amazonaws.com/get-items', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  // Automatically added sleep
+  sleep(1)
 }
